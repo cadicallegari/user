ee46e454d0ca187a8e01cd7cba809413c511973f
Claudinei Callegari <cadicallegari@gmail.com>
2022-08-01 19:03:02 +0200

    Add base service definitions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cadicallegari/user
+
+go 1.18
---
+++ user.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"context"
+	"time"
+)
+
+const (
+	DefaultPerPage = 25
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Nickname  string    `json:"nickname"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	Country   string    `json:"country"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type ListOptions struct {
+	PerPage uint64 `schema:"per_page"`
+	Cursor  string `schema:"cursor"`
+	Role    string `schema:"role"`
+	Search  string `schema:"search"`
+}
+
+func NewListOptions() *ListOptions {
+	return &ListOptions{
+		PerPage: uint64(DefaultPerPage),
+	}
+}
+
+type List struct {
+	Users      []*User `json:"users"`
+	Total      uint64  `json:"total"`
+	PrevCursor *string `json:"prev_cursor"`
+	NextCursor *string `json:"next_cursor"`
+}
+
+type Service interface {
+	List(context.Context, *ListOptions) (*List, error)
+	Get(_ context.Context, id string) (*User, error)
+	Create(context.Context, *User) (*User, error)
+	Update(context.Context, *User) (*User, error)
+	Delete(context.Context, *User) error
+}
